da6a9ffa732162a285262f8a988d975d899cf2ae
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-05-17 18:31:20 +0200

    Added ability to list scores by dates

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.utils.dateparse import parse_date
 from rest_framework import viewsets, generics
 
 from leaderboard.models import Score
@@ -17,9 +18,20 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class ScoreListCreateView(generics.ListCreateAPIView):
-    queryset = Score.objects.all().order_by('-points')
     serializer_class = ScoreSerializer
     permission_classes = [IsValidToken]
+
+    def get_queryset(self):
+        queryset = Score.objects.all().order_by('-points')
+        score_date = self.request.query_params.get('score_date', None)
+        if score_date is not None:
+            try:
+                parsed_date = parse_date(score_date)
+                if parsed_date:
+                    queryset = queryset.filter(score_ts__date=parsed_date)
+            except ValueError as e:
+                raise e  # Optionally, handle invalid date format
+        return queryset
 
 
 class ScoreDetailView(generics.RetrieveUpdateDestroyAPIView):
