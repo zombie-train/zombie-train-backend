0846719c5f95b00be3a04cd6b9474428a8b72601
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-05-25 15:50:56 +0200

    Registered models in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from api.models import Region
+
 # Register your models here.
+admin.site.register(Region)
--- score/admin.py
+++ score/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from score.models import Score, Leaderboard
+
 # Register your models here.
+admin.site.register(Score)
+admin.site.register(Leaderboard)
