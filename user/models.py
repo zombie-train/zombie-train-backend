4bf0cc4628e2a0dbb0b6dbab8c895b46f72f06b4
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-05-29 00:11:31 +0200

    Added necessary fields to GameUser model

--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 from api.models import Region
 from api.utils import get_default_region
+from score.models import Score
 
 
 class GameUser(AbstractUser):
@@ -12,6 +13,14 @@ class GameUser(AbstractUser):
                                        default=get_default_region,
                                        null=True, blank=True,
                                        related_name='current_users')
+    current_region_score = models.ForeignKey(Score,
+                                             on_delete=models.SET_NULL,
+                                             null=True, blank=True,
+                                             related_name='user_current_region_scores')
+
+    is_banned = models.BooleanField(default=False)
+    is_cheater = models.BooleanField(default=False)
+    is_suspicious = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
