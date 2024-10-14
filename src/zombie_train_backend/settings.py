8b1dc6036afa753fb2ccf219774e3efbc96a1797
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-10-14 17:59:40 +0200

    Added storage for static files

--- src/zombie_train_backend/settings.py
+++ src/zombie_train_backend/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -174,6 +175,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = []
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- update_image.sh
+++ update_image.sh
@@ -1,3 +1,3 @@
-docker build -t zombie-train-backend:latest .
+docker build -t zombie-train-backend:latest . -f Dockerfile.prod
 
 # docker push zombie-train-backend:latest
