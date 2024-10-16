562b06c38cfb67e00df0e1f8f1797aa3fe85299c
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-10-16 10:45:26 +0200

    Fixed env variable for database

--- src/zombie_train_backend/settings.py
+++ src/zombie_train_backend/settings.py
@@ -115,20 +115,17 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    "dev": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
     "prod": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     },
 }
-
+DATABASES["dev"] = DATABASES["default"]
+DATABASES["stage"] = DATABASES["prod"]
 DATABASES["default"] = DATABASES[ENV]
 
 # Password validation
