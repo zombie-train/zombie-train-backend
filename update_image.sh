aed2e0986a8f1b644c43597ade394bd3b3756a1c
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-10-16 10:43:56 +0200

    Update script for updating image

--- update_image.sh
+++ update_image.sh
@@ -1,3 +1,20 @@
-docker build -t zombie-train-backend:latest . -f Dockerfile.prod
+#!/bin/bash
 
-# docker push zombie-train-backend:latest
+container_registry="zombietrain.azurecr.io"
+name="zombie-train-backend"
+version="latest"
+
+# Build the Docker image
+docker build -t $container_registry/$name:$version . -f Dockerfile.prod
+
+# Check if the push flag is set
+if [ "$1" = "--push" ]; then
+    echo "Pushing the image to the registry..."
+    # if not logged in, log in
+    
+    az login
+    az acr login --name $container_registry
+    docker push $container_registry/$name:$version
+else
+    echo "Image built successfully. Use --push flag to push to registry."
+fi
