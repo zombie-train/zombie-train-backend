8d72387f7c41c979b9518e49900381dbd30a3f02
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-10-28 16:09:27 +0100

    Fixed issue with default region

--- src/api/utils.py
+++ src/api/utils.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 def get_default_region():
     default_region = None
     try:
-        default_region = Region.objects.first() if Region.objects.exists() else None
+        default_region = Region.objects.first()
     except OperationalError as e:
         logger.warning(str(e))
     except ProgrammingError as e:
